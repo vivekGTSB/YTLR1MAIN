80734f2e6f839a33833b430b5bc5770de3006334
PPSPC\pwint <pwintphyusin1996@gmail.com>
2025-06-19 16:51:38 +0800

    a DELETE

--- YTLWebApplication/A/AccessDenied.aspx.vb
+++
@@ -1,8 +0,0 @@
-Partial Public Class AccessDenied
-    Inherits System.Web.UI.Page
-    
-    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        SecurityHelper.LogSecurityEvent("ACCESS_DENIED", "Access denied page accessed")
-    End Sub
-    
-End Class
